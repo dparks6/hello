593a5be0a83c2cce14b0173e260b27e4cb15ff0c
Derek Parks <dparks6@huskers.unl.edu>
2022-08-25 17:58:10 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Derek Parks
+ *         dparks6@huskers.unl.edu
+ * Date: 2022/8/25
+ *
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Derek Parks\n");
+
+  return 0;
+}
